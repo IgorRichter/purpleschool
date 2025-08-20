b2100fb25782e8d3262e134dcbf83493e0787a5a
Igor Richter <i.rixter201572@gmail.com>
2025-08-20 12:37:12 +0300

    delete style layout x2

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Одно из самых вкусных кофе в городе!</Text>
-      <Text style={styles.text}>Свежие зёрна, настоящая арабика и бережная обжарка</Text>
+    <View>
+      <Text>Одно из самых вкусных кофе в городе!</Text>
+      <Text>Свежие зёрна, настоящая арабика и бережная обжарка</Text>
       <Button title='Начать' />
       <StatusBar style="auto" />
     </View>
